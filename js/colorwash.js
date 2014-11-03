8b226da4b68a187900cfb2fafe9c324b25ff117c
J Cornelius <github@jcornelius.com>
2014-11-03 15:52:53 -0500

    pre-production updates
    Signed-off-by: J Cornelius <github@jcornelius.com>

--- js/colorwash.js
+++ js/colorwash.js
@@ -30,12 +30,12 @@ var istep = 1 - step;
 var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
 var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
 var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-var color1 = "#"+((r1 << 16) | (g1 << 8) | b1).toString(16);
+var color1 = ((r1 << 16) | (g1 << 8) | b1).toString(16);
 
 var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
 var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
 var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-var color2 = "#"+((r2 << 16) | (g2 << 8) | b2).toString(16);
+var color2 = ((r2 << 16) | (g2 << 8) | b2).toString(16);
 
  $('#lldfl').css({
    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
