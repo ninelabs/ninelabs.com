d2655f2e9830347fde005e9607d62a042d3cdeda
J Cornelius <github@jcornelius.com>
2015-01-21 16:56:54 -0500

    Updated sessions page. Also split out the Partners and Team on the Experience page so it's clearer who would be in the sessions.
    Signed-off-by: J Cornelius <github@jcornelius.com>

--- _plugins/reading_time.rb
+++ _plugins/reading_time.rb
@@ -7,7 +7,6 @@
 module ReadingTimeFilter
   def reading_time( input )
     words_per_minute = 180
-
     words = input.split.size;
     minutes = ( words / words_per_minute ).floor
     minutes_label = minutes === 1 ? " minute" : " minutes"
